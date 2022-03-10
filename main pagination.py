0feacd5f56d9eaa3e847884c827252d03159851a
SEMBARANG <ahmadsulthoni87@gmail.com>
2022-03-11 06:12:20 +0700

    membuat pagination 2

--- main pagination.py
+++ main pagination.py
@@ -23,10 +23,10 @@ def get_total_pages(searches):
 
     res = requests.get(url, params=params, headers=headers)
 
-    soup = BeautifulSoup(res.text, 'html.parser')
+    soup = BeautifulSoup(res.content, 'html.parser')
 
     pages = []
-    headers_contents = soup.find_all('div','col-sm-14 col-md-16 col-lg-14 text-center').find_all('a')
+    headers_contents = soup.find('div','col-sm-14 col-md-16 col-lg-14 text-center').find_all('a')
     for i in headers_contents:
         pages.append(int(i.text))
 
@@ -36,45 +36,66 @@ def get_total_pages(searches):
     print(total_pages)
     return total_pages
 
-'''
-#scraping prosess
-
-headers_contents = soup.find_all('div','row businessCapsule--mainRow')
-
-for content in headers_contents:
-    title = content.find('h2','businessCapsule--name text-h2').text
-    classification = content.find('span','businessCapsule--classification').text
-    telephone = content.find('span','business--telephoneNumber').text
-    link_web = base_url + content.find('div','businessCapsule--titSpons').find('a')['href']
-
-    #sorting data
-    final_data = {
-        'title' : title,
-        'classification' : classification,
-        'telephone' : telephone,
-        'link web' : link_web,
+def get_all_item(searches,pages):
+    hotelist = []
+    params = {
+        'keywords': 'Restaurants',
+        'location': 'london',
+        'scrambleSeed': '1290094646',
+        'keywords': searches,
+        '_pgn': pages,
     }
 
-    #mencetak datanya
-    #print(final_data)
-    result.append(final_data)
-print('Jumlah datanya adalah', len(result))
+    res = requests.get(url,params=params, headers=headers)
 
-try :
-    os.mkdir('json_result')
-except FileExistsError:
-    pass
+    soup = BeautifulSoup(res.text,'html.parser')
+    hotelist = []
+    results = soup.find('div', {'class':'row results--row results--capsuleList'})
+    contents = results.find_all('div', {'class':'row businessCapsule--mainRow'})
 
-with open('json_result/final_data.json','w+') as json_data:
-    json.dump(result, json_data)
-print('json created')
+    for content in contents:
+        title = content.find('h2', 'businessCapsule--name text-h2').text
+        classification = content.find('span', 'businessCapsule--classification').text
+        telephone = content.find('span', 'business--telephoneNumber').text
+        link_web = base_url + content.find('div', 'businessCapsule--titSpons').find('a')['href']
 
-#create csv
+        # sorting data
+        final_data = {
+            'title': title,
+            'classification': classification,
+            'telephone': telephone,
+            'link web': link_web,
+        }
+        hotelist.append(final_data)
+    return hotelist
 
-df = pd.DataFrame(result)
-df.to_csv('yell_data.csv', index=False)
-df.to_excel('yell_data.xlsx', index=False)
-'''
+def output(searches, final_result):
+    # is using to print csv or excel ffile
+    # parameter final_result is the result from scraping and pagination
+    df = pd.DataFrame(final_result)
+    df.to_csv(f'{searches}.csv', index=False)
+    df.to_excel('yell_data.xlsx', index=False)
+
+def main(searches):
+    final_result = []
+
+    total_pages = get_total_pages(searches)
+    for page in range (total_pages):
+        page += 1
+        print(f'Scraping halaman ke:{page}')
+        products = get_all_item(searches, page)
+        final_result += products
+
+    #the prosessing data here(final_result)
+    total_data = len(final_result)
+    print('Ini adalah total halaman yang sudah di scraping'. format(total_data))
+
+    #generate csv used output(out of looping)
+    output(searches, final_result)
+
+if __name__ == '__main__':
+    searches = ('restaurants')
+    main(searches)
 #data created
 print('Data Created Succes')
 
